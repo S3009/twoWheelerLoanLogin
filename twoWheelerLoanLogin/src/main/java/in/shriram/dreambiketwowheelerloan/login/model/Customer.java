fd6d8db9821ea8419e5ffaf152b380dca1e71b89
priyankabagde2610 <priyankabagde2610@gmail.com>
2025-03-10 09:45:29 +0530

    added

--- twoWheelerLoanLogin/src/main/java/in/shriram/dreambiketwowheelerloan/login/model/Customer.java
+++ twoWheelerLoanLogin/src/main/java/in/shriram/dreambiketwowheelerloan/login/model/Customer.java
@@ -2,12 +2,13 @@ package in.shriram.dreambiketwowheelerloan.login.model;
 
 
 import jakarta.persistence.CascadeType;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 
